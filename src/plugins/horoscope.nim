0470e1cea25dd47564fb3fb9c0ce01dd42a9f658
snus-kin <tcarroll@snufk.in>
2020-07-05 21:38:11 +0100

    Add quote and error handling

--- src/plugins/horoscope.nim
+++ src/plugins/horoscope.nim
@@ -7,12 +7,13 @@ proc horoscope(plugin: Plugin, cmd: CmdData) {.plugincallback.} =
   else:
     let sign = cmd.params[0]
     let client = newHttpClient()
-    let resp = client.getContent(fmt"https://my.horoscope.com/astrology/free-daily-horoscope-{sign}.html")
-    client.close()
-    
-    var text = resp.parseHtml.findAll("p")[0].innerText
-
-    cmd.returned.add text
+    try:
+      let resp = client.getContent(fmt"https://my.horoscope.com/astrology/free-daily-horoscope-{sign}.html")
+      client.close()
+      var text = "> " & resp.parseHtml.findAll("p")[0].innerText
+      cmd.returned.add text
+    except HttpRequestError:
+      cmd.returned.add "Could not find horoscope"
 
 pluginLoad:
   discard
